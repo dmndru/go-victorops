d2221bcf053ed89bcc9baed0c26d0cf2a6a0b40d
Dmitriy Loginov <dmndru@gmail.com>
2017-02-17 08:42:38 +0300

    implement method CreateOncallOverride

--- schedule.go
+++ schedule.go
@@ -1,30 +1,35 @@
 package victorops
 
+import (
+	"encoding/json"
+	"time"
+)
+
 // Schedule current schedule
 type Schedule struct {
-	Oncall         string `json:"oncall,omitempty"`
-	OverrideOncall string `json:"overrideoncall,omitempty"`
-	PolicyType     string `json:"policyType,omitempty"`
-	RotationName   string `json:"rotationName,omitempty"`
-	ShiftName      string `json:"shiftName,omitempty"`
-	ShiftRoll      int    `json:"shiftRoll,omitempty"`
-	Rolls          []Roll `json:"rolls,omitempty"`
+	Oncall         string    `json:"oncall,omitempty"`
+	OverrideOncall string    `json:"overrideoncall,omitempty"`
+	PolicyType     string    `json:"policyType,omitempty"`
+	RotationName   string    `json:"rotationName,omitempty"`
+	ShiftName      string    `json:"shiftName,omitempty"`
+	ShiftRoll      time.Time `json:"shiftRoll,omitempty"`
+	Rolls          []Roll    `json:"rolls,omitempty"`
 }
 
 // Roll is shcedule roll
 type Roll struct {
-	Change int    `json:"change,omitempty"`
-	Until  int    `json:"until,omitempty"`
-	Oncall string `json:"oncall,omitempty"`
-	IsRoll bool   `json:"isroll,omitempty"`
+	Start  time.Time `json:"change,omitempty"`
+	End    time.Time `json:"until,omitempty"`
+	Oncall string    `json:"oncall,omitempty"`
+	IsRoll bool      `json:"isroll,omitempty"`
 }
 
 // Override describes oncall overrides
 type Override struct {
-	Orig  string `json:"orig,omitempty"`
-	Over  string `json:"over,omitempty"`
-	Start int    `json:"start,omitempty"`
-	End   int    `json:"end,omitempty"`
+	Orig  string    `json:"orig,omitempty"`
+	Over  string    `json:"over,omitempty"`
+	Start time.Time `json:"start,omitempty"`
+	End   time.Time `json:"end,omitempty"`
 }
 
 // TeamSchedule is api response struct
@@ -32,6 +37,12 @@ type TeamSchedule struct {
 	Team      string     `json:"team"`
 	Schedules []Schedule `json:"schedule"`
 	Overrides []Override `json:"overrides"`
+}
+
+// TeamOverride is a pyaload for creating override request
+type teamOverride struct {
+	FromUser string `json:"fromUser"`
+	ToUser   string `json:"toUser"`
 }
 
 // ResponseResult is result of an api request
@@ -61,5 +72,12 @@ func (client *Client) TeamOncallSchedule(team string) (TeamSchedule, error) {
 
 // CreateOncallOverride replaces a currently on-call user on the team with another
 func (client *Client) CreateOncallOverride(fromUser, toUser, team string) (ResponseResult, error) {
-	return ResponseResult{}, nil
+	var resp ResponseResult
+	values := teamOverride{fromUser, toUser}
+	jsonData, err := json.Marshal(values)
+	if err != nil {
+		return resp, err
+	}
+	err = client.sendRequest("PATCH", "api-public/v1/team/"+team+"/oncall/user", jsonData, &resp)
+	return resp, err
 }
