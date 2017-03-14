ab487eef6e4d61b0455cf54ed77e0e7a1445cd61
Dmitriy Loginov <dmndru@gmail.com>
2017-03-14 16:40:52 +0300

    initial implementation of Teams group of methods

--- team.go
+++ team.go
@@ -1,5 +1,7 @@
 package victorops
 
+import "encoding/json"
+
 // Team struct describes a team
 type Team struct {
 	Name          string `json:"name"`
@@ -15,4 +17,64 @@ type Team struct {
 type TeamListResp struct {
 	Teams   []Team `json:"teams"`
 	SelfURL string `json:"_selfUrl"`
+}
+
+// TeamMember member of a team
+type TeamMember struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	UserName  string `json:"username"`
+	Version   int    `json:"version"`
+	Verified  string `json:"verified"`
+	SelfURL   string `json:"_selfUrl"`
+}
+
+// TeamMembersResp contains response to GetTeamMembers method
+type TeamMembersResp struct {
+	Members []TeamMember `json:"teams"`
+	SelfURL string       `json:"_selfUrl"`
+}
+
+// GetTeams Get a list of teams for your organization
+func (client *Client) GetTeams() (TeamListResp, error) {
+	var resp TeamListResp
+	err := client.sendRequest("GET", "api-public/v1/team", nil, &resp)
+	return resp, err
+}
+
+// AddTeam Add a team to your organization
+func (client *Client) AddTeam(name string) (Team, error) {
+	var team Team
+	values := map[string]string{"name": name}
+	jsonData, err := json.Marshal(values)
+	if err != nil {
+		return team, err
+	}
+	err = client.sendRequest("POST", "api-public/v1/team", jsonData, &team)
+	return team, err
+}
+
+// RemoveTeam Remove a team from your organization
+func (client *Client) RemoveTeam(name string) error {
+	err := client.sendRequest("DELETE", "api-public/v1/team/"+name, nil, nil)
+	return err
+}
+
+// GetTeam Get the information for the specified team
+func (client *Client) GetTeam(name string) (Team, error) {
+	var team Team
+	err := client.sendRequest("GET", "api-public/v1/team"+name, nil, &team)
+	return team, err
+}
+
+// UpdateTeam Update the designated team
+func (client *Client) UpdateTeam(name, newName string) (Team, error) {
+	var team Team
+	values := map[string]string{"name": newName}
+	jsonData, err := json.Marshal(values)
+	if err != nil {
+		return team, err
+	}
+	err = client.sendRequest("PUT", "api-public/v1/team"+name, jsonData, &team)
+	return team, err
 }
